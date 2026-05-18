f8772df4c01e5aba2132573c95c712928f1a3f24
evelynklert <eklertia@student.touro.edu>
2026-05-18 02:26:29 +0000

    edits3

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript Documentfunction toggleFilters() {
+function openFilters() {
+  document.getElementById("filterScreen").classList.add("active");
+}
+
+function closeFilters() {
+  document.getElementById("filterScreen").classList.remove("active");
+}
